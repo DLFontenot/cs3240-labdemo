80a80e326728a162bdeaf459a3454e49cdbdca54
DLFontenot <DanFontenot14@gmail.com>
2017-02-20 14:18:50 -0500

    Moved greeting() to helper.py

--- hello.py
+++ hello.py
@@ -5,11 +5,10 @@ Created on Mon Feb 20 10:55:07 2017
 @author: dlf4mf
 """
 
-def greeting(msg):
-    print(msg)
+import helper
 
 def main():
-    greeting("hello")
+    helper.greeting("hello")
     
 if __name__ == "__main__":
     main()
---
+++ helper.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 10:55:07 2017
+
+@author: dlf4mf
+"""
+
+
+def greeting(msg):
+    print(msg)
