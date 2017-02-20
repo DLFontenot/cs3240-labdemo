8a92322f04b1abd71bdf38de4587ab3cac2e8eac
DLFontenot <DanFontenot14@gmail.com>
2017-02-20 14:16:45 -0500

    added greeting() to hello.py and created intellij project

--- hello.py
+++ hello.py
@@ -2,11 +2,14 @@
 """
 Created on Mon Feb 20 10:55:07 2017
 
-@author: Fonti
+@author: dlf4mf
 """
 
+def greeting(msg):
+    print(msg)
+
 def main():
-    print("Hello World!")
+    greeting("hello")
     
 if __name__ == "__main__":
     main()
